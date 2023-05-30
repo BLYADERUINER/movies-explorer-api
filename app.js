456d908b3972819d090f5e20e4dcd1fba49db05e
Dmitry Alekseev <Ezzy-Way@yandex.ru>
2023-05-31 00:53:28 +0400

    added/cors

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const { errors } = require('celebrate');
 const helmet = require('helmet');
+const { errors } = require('celebrate');
 
-// const cors = require('./middlewares/cors');
+const cors = require('./middlewares/cors');
 const { PORT, MONGOOSE_URL, limiter } = require('./utils/config');
 const handleErrors = require('./middlewares/handlerErrors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -13,7 +13,7 @@ const router = require('./routes/index');
 const app = express();
 mongoose.connect(MONGOOSE_URL);
 
-// app.use(cors);
+app.use(cors);
 app.use(helmet());
 app.use(limiter);
 app.use(express.json());
